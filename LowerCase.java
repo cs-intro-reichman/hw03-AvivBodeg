b39b35ddae4b4302241a6de740299f7a83f2eb0b
Aviv Bodeg <AVIVBodeg@gmail.com>
2024-01-05 18:10:06 +0200

    Calender fixes

--- LowerCase.java
+++ LowerCase.java
@@ -1,22 +1,24 @@
-/** String processing exercise 1. */
+/**
+ * String processing exercise 1.
+ */
 public class LowerCase {
-    public static void main(String[] args) {  
+    public static void main(String[] args) {
         String str = args[0];
         System.out.println(lowerCase(str));
     }
 
-   /**
-    * Returns a string which is identical to the original string, 
-    * except that all the upper-case letters are converted to lower-case letters.
-    * Non-letter characters are left as is.
-    */
+    /**
+     * Returns a string which is identical to the original string,
+     * except that all the upper-case letters are converted to lower-case letters.
+     * Non-letter characters are left as is.
+     */
     public static String lowerCase(String s) {
         String res = "";
-        for (int i = 0; i< s.length() ; i++){
+        for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c >= 'A' && c <= 'Z'){
+            if (c >= 'A' && c <= 'Z') {
                 res += (char) (c + 32);
-            } else{
+            } else {
                 res += c;
             }
         }
