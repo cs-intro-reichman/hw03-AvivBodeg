62e214b4c69d42618ec0ec90b1f332342ff8fe86
Aviv Bodeg <AVIVBodeg@gmail.com>
2024-01-05 15:16:06 +0200

    autograding update

--- LowerCase.java
+++ LowerCase.java
@@ -11,7 +11,15 @@ public class LowerCase {
     * Non-letter characters are left as is.
     */
     public static String lowerCase(String s) {
-        // Replace the following statement with your code
-        return null;
+        String res = "";
+        for (int i = 0; i< s.length() ; i++){
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z'){
+                res += (char) (c + 32);
+            } else{
+                res += c;
+            }
+        }
+        return res;
     }
 }
